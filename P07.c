d99cd2bea2d6a94acf88812ec08ff60d1e1e92b2
e-stella-r <37545907+e-stella-r@users.noreply.github.com>
2018-06-24 22:34:16 -0300

    Add files via upload

---
+++ P07.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int ackerman(int m, int n)
+{
+	if(m==0) return n + 1;
+	else if(m>0 && n==0) return ackerman(m-1, 1);
+	else if(m>0 && n>0)  return ackerman(m-1,ackerman(m, n-1));
+  	return 0;
+}
+
+int main(){
+	int i, j;
+		printf("Digite o valor desejado de m:\n>");
+		scanf("%d",&i);
+		printf("Digite o valor desejado de n:\n>");
+		scanf("%d",&j);
+		printf("Ackermann Function: (%d, %d) = %d\n", i, j, ackerman(i,j));
+	return 0; 
+}
+
