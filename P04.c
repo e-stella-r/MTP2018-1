6dc2e05dc03bde86360c7d1fdc041f906b74b5d2
e-stella-r <37545907+e-stella-r@users.noreply.github.com>
2018-06-24 22:28:36 -0300

    Add files via upload

---
+++ P04.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int i=0, c=0;
+	char bits[256];  
+	scanf("%s", bits);
+	getchar();
+	
+	while(bits[i])
+	{
+		if(bits[i] >= 48 && bits[i] <= 57) 
+		{
+			bits[i] = bits[i] - '0'; 	
+			c=c*10+bits[i];
+		}
+		i++;
+	}
+	printf("O numero digitado e: %d", c);
+	getchar();
+	
+	return 0;
+}
