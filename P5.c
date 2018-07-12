87ae69797719c69d4d8c0c8f69550cbe706467a1
e-stella-r <37545907+e-stella-r@users.noreply.github.com>
2018-07-12 08:47:49 -0300

    Update P5.c

--- P5.c
+++ P5.c
@@ -1,43 +1,52 @@
-//Stella Aragão Araújo 11721ETE014
-
+//Stella Aragão Araújo
+//11721ETE014
 #include <stdio.h>
+#include <locale.h>
 
-int main ()
+
+int main()
 {
-	int x, i, number [64], *num;
-	char mensagem[256], *msg;
-	for (i=0;i<256; i++)
-	mensagem[i]='\0';
-	printf("\tCodificar\n\tDecodificar\n\tEscolha uma:");
+	int x , i, cod[64], *numeros;
+	char msg[256], *mensagem;
+
+	for(i = 0; i < 256; i++) msg[i] = '\0';
+	printf("\t1. Codificar\n\t2. Decodificar\n\n\t");
 	scanf("%d",&x);
 	getchar();
-	switch(x) {
+
+	switch(x)
+	{
+
 		case 1:{
-			printf("Digite a frase com maximo de 255 caracteres:\n\n\t");
-			gets(mensagem);
-			char*msg=mensagem;
-			printf("\n\t A frase codificada eh: \n\t\t");;
-			for(i=0;mensagem[i]; i++){
-				num=(int*) msg;
-				if (num[i]!=0)
-				printf("%d,",num[i]);
-				if (num[i+1]!=0)
-				printf(",");}
-				break;
-			}
-			case 2:
-				{
-					printf("\t Digite os numeros com maximo de 64, sendo o ultimo(64) entre 1 e ""4095!!! \n \t obs: Apos inserir o numero digite 0 \n\n");
-					for (i=0; i<64; i++)
-					{
-						printf("\t\n %d: \t", i+1);
-						scanf("%d", &number[i]);
-						msg = (char*)number;
-						if(number[i]==0)
-						break;
-					}
-					printf("\n\n\t A frase decodificada eh: %s", msg);
-				}
+			printf("\n\tDigite uma frase (ate 255 caracteres): \n\t");
+			scanf("%s", msg);
+			char *mensagem= msg;
+			printf("\n\tFrase codificada:\n\t");
+
+			for(i = 0; msg[i] ;i++)
+			{
+				numeros = (int*)mensagem;
+				if(numeros[i]!= 0) printf("%d",numeros[i]);
+				if(numeros[i+1]!= 0) printf(", ");}
+			break;
 		}
-		return 0;
+
+		case 2:
+		{
+			printf("\t\nDigite os numeros (max.=64), (ultimo numero >=1 e <=4095).\n   \t\t Para finalizar a entrada digite 0\n\n");
+
+			for(i = 0; i < 64; i++)
+			 {
+				printf("n%d:\t ", i+1);
+				scanf("%d", &cod[i]);
+				mensagem = (char*)cod;
+				if(cod[i]==0)
+				break;
+			 }
+
+			 printf("\n\n\tFrase decodificada: %s", mensagem);
+	    }
+
 	}
+	return 0;
+}
